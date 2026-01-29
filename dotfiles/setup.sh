5298bc264839c3b037b9f5353c7608b3d6b9d320
Zahan Malkani <zahan@openai.com>
2026-01-29 13:51:23 -0800

    install gh

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -29,3 +29,21 @@ for src in "$DOTFILES_DIR"/.*(N); do
     exit 1
   fi
 done
+
+# install gh (if needed)
+if ! command -v gh >/dev/null 2>&1; then
+    if ! command -v curl >/dev/null 2>&1; then
+        sudo apt-get update
+        sudo apt-get install -y curl
+    fi
+
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
+        | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y gh
+fi
